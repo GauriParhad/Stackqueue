84b44d5546aedbe9a7c77af49853f3d1533da330
Gaurip10 <thegauriparhad@gmail.com>
2021-08-20 23:39:19 +0530

    added: Pooping all elements

--- src/main/java/StackQueue.java
+++ src/main/java/StackQueue.java
@@ -1,3 +1,4 @@
+import jdk.jfr.StackTrace;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -16,4 +17,22 @@ public class StackQueue {
 
     }
 }
+}
+@Test
+//pop all the elements//
+public class Poppingstack{
+    public void poppingstack(){
+        MyNode<Integer>myFirstNode=new MyNODE<>(key:56);
+        MyNode<Integer>mySecondNode=new MyNODE<>(key:30);
+        MyNode<Integer>myThirdNode=new MyNODE<>(key:70);
+        myFirstNode.setNext(mySecondNode);
+        mySecondNode.setNext(myThirdNode);
+        myLinkedList.pop();
+        myLinkedList.pop();
+        myLinkedList.pop();
+        Assert.assertTrue(res
+        boolean result=myFirstNode.getNext().equals(mySecondNode) && mySecondNode.getNext().equals(myThirdNode);
+        Assert.assertTrue(result);
+
+    }
 }
