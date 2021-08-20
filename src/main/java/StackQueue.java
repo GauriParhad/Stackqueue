7bf3f767d96873019eed34575c7ad064316367d1
Gaurip10 <thegauriparhad@gmail.com>
2021-08-20 23:31:57 +0530

    added: ability to create stack

--- src/main/java/StackQueue.java
+++ src/main/java/StackQueue.java
@@ -3,4 +3,17 @@ import org.junit.Test;
 
 
 public class StackQueue {
+    @Test
+    public void given3NumbersWhenLinkedShouldPassedLinkedListTest(){
+        MyNode<Integer>myFirstNode=new MyNODE<>(key:56);
+        MyNode<Integer>mySecondNode=new MyNODE<>(key:30);
+        MyNode<Integer>myThirdNode=new MyNODE<>(key:70);
+        myFirstNode.setNext(mySecondNode);
+        mySecondNode.setNext(myThirdNode);
+        boolean result=myFirstNode.getNext().equals(mySecondNode) && mySecondNode.getNext().equals(myThirdNode);
+        StackQueue.pop();
+        Assert.assertTrue(result);
+
+    }
+}
 }
