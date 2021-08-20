86c9bed66019741632ab2eda7824bc4bb53faacf
Gaurip10 <thegauriparhad@gmail.com>
2021-08-20 23:42:14 +0530

    Added: Given 3 numbers added to quque

--- src/main/java/StackQueue.java
+++ src/main/java/StackQueue.java
@@ -36,3 +36,18 @@ public class Poppingstack{
 
     }
 }
+}
+@Test
+
+public class MyQueue{
+    public void 3numbersaddedtoqueue{
+        MyNode<Integer>myFirstNode=new MyNODE<>(key:56);
+        MyNode<Integer>mySecondNode=new MyNODE<>(key:30);
+        MyNode<Integer>myThirdNode=new MyNODE<>(key:70);
+        myFirstNode.setNext(mySecondNode);
+        mySecondNode.setNext(myThirdNode);
+        boolean result=myFirstNode.getNext().equals(mySecondNode) && mySecondNode.getNext().equals(myThirdNode);
+        Assert.assertTrue(result);
+
+    }
+}
